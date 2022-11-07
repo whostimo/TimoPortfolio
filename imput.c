afed48c206f0895e168da88a40bcbf8309ddae77
Your Name <you@example.com>
2022-11-07 11:09:58 -0500

    Added input and string

---
+++ imput.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+	int var, var2;
+       	printf("Type in 2 integers: ");
+	scanf("%i%i", &var, &var2);
+	printf("You sum of the numbers is %i\n", var + var2);
+	return 0;
+}
---
+++ string.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main () {
+	char name[30];
+	printf("Enter the name of your character: ");
+	scanf("%s", &name);
+	printf("Your character's name is %s\n", name);
+	return 0;
+}
